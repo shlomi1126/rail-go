3ccfc08445b87f9e2deecd45639de07ff44ce1f5
shlomi1126 <shlomi1126@gmail.com>
2025-02-20 22:51:46 +0200

    update log level

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = 6000
 
 	updates := bot.GetUpdatesChan(u)
 
